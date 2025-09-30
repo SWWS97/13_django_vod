f968ce885f77b6bbc0eb1857ca7207ea2ab9fd9c
SWWS97 <parkmon26@gmail.com>
2025-09-30 17:49:54 +0900

    deleted vercel setting

--- blog/api/index.py
+++
@@ -1,7 +0,0 @@
-# api/index.py  — Django WSGI 엔트리
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-app = get_wsgi_application()  # <-- Vercel Python은 WSGI callable인 'app'를 찾습니다.
