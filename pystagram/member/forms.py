66c07b88ef0f585f9b03f742bb8a0decbdab5c8f
SWWS97 <parkmon26@gmail.com>
2025-10-06 22:08:59 +0900

    add SignupForm & templates

---
+++ pystagram/member/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+# password1, password2를 __init__을 오버라이딩해서 만들어 줌
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # class_default_fields = ("password1", "password2")
+
+        for field in ("password1", "password2"):
+            self.fields[field].widget.attrs["class"] = "form-control"
+            self.fields[field].widget.attrs["placeholder"] = "password"
+
+            if field == "password1":
+                self.fields[field].label = "비밀번호"
+            else:
+                self.fields[field].label = "비밀번호 확인"
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email", "nickname", )
+        labels = {
+            "email": "이메일",
+            "nickname": "닉네임",
+        }
+        widgets = {
+            "email": forms.EmailInput(
+                attrs={
+                    "placeholder": "example@example.com",
+                    "class": "form-control",
+                }
+            ),
+            "nickname": forms.TextInput(
+                attrs={
+                    "placeholder": "닉네임",
+                    "class": "form-control",
+                }
+            )
+        }
